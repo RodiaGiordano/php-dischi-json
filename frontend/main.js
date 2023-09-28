71f87a9e2afd7be93eade36de945a0d79e3e91c7
RodiaGiordano <rodiagiordano93@gmail.com>
2023-09-28 19:14:29 +0200

    feat: add bonus

--- frontend/main.js
+++ frontend/main.js
@@ -4,7 +4,25 @@ createApp({
   data() {
     return {
       discList: [],
+      show: false,
+      bigCard: {
+        poster: "",
+        title: "",
+        author: "",
+        year: "",
+      },
     };
+  },
+
+  methods: {
+    onlyThis(newCard) {
+      this.show = true;
+
+      this.bigCard.poster = newCard.poster;
+      this.bigCard.title = newCard.title;
+      this.bigCard.author = newCard.author;
+      this.bigCard.year = newCard.year;
+    },
   },
 
   mounted() {
@@ -12,6 +30,7 @@ createApp({
       .get("http://localhost/php-dischi-json/backend/data.php")
       .then((response) => {
         this.discList = response.data;
+        console.log(this.discList);
       });
   },
 }).mount("#app");
