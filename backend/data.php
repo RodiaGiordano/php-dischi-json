c5e32e87bb406b613daad3e01ec0a5367c6cc2aa
RodiaGiordano <rodiagiordano93@gmail.com>
2023-09-28 18:03:01 +0200

    feat: import json in data.php

--- backend/data.php
+++ backend/data.php
@@ -4,7 +4,9 @@ header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
 header("Access-Control-Allow-Headers: X-Requested-With");
 
 
-$discList = file_get_contents('./')
+$json_discList = file_get_contents('./data.json');
+
+$discList = json_decode($json_discList);
 
 header('Content-Type: application/json');
 
